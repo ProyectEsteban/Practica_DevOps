983cce5a6742402f7cd62d83ff877efa52f021f0
FerSteban <Ferhdez0918esteban@gmail.com>
2023-10-27 14:34:29 -0600

    Practica de Mes

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  nombre = 'Tienda de Autos';
+  ocupacion = 'Autos Audi';
+  infoPersonal = [
+  ];
+  estadisticas = [
+  ]
+  title = 'portafolio_angular';
+}
